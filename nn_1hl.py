e68ecdd034d384dfe564383303c9ea7d66c42060
Angelo Monteiro <afrmonteiro@gmail.com>
2022-03-17 12:22:26 +0000

    first commit

---
+++ nn_1hl.py
@@ -0,0 +1,77 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+from public_tests import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+
+class NN1HiddenLayer:
+    def __init__(self, name, X, Y, hidden_layer_size):
+        """
+        Arguments:
+        X -- input dataset of shape (input size, number of examples)
+        Y -- labels of shape (output size, number of examples)
+        Instance Variables:
+        n_x -- the size of the input layer
+        n_h -- the size of the hidden layer
+        n_y -- the size of the output layer
+        params -- python dictionary containing your parameters:
+            W1 -- weight matrix of shape (n_h, n_x)
+            b1 -- bias vector of shape (n_h, 1)
+            W2 -- weight matrix of shape (n_y, n_h)
+            b2 -- bias vector of shape (n_y, 1)
+        """
+        self.name = name
+        self.n_x = X.shape[0]
+        self.n_h = hidden_layer_size
+        self.n_y = Y.shape[0]
+        self.parameters = {}
+        self.initialize_parameters()
+        self.cache = {}
+        self.forward_propagation(X)
+
+    def initialize_parameters(self):
+        W1 = np.random.randn(self.n_h, self.n_x) * 0.01
+        b1 = np.zeros((self.n_h, 1))
+        W2 = np.random.randn(self.n_y, self.n_h) * 0.01
+        b2 = np.zeros((self.n_y, 1))
+        self.parameters = {"W1": W1,
+                           "b1": b1,
+                           "W2": W2,
+                           "b2": b2}
+
+    def forward_propagation(self, X):
+        """
+        Argument:
+        X -- input data of size (n_x, m)
+        parameters -- python dictionary containing your parameters (output of initialization function)
+
+        Returns:
+        A2 -- The sigmoid output of the second activation
+        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+        """
+        # Retrieve each parameter from the dictionary "parameters"
+        W1 = self.parameters["W1"]
+        b1 = self.parameters["b1"]
+        W2 = self.parameters["W2"]
+        b2 = self.parameters["b2"]
+
+        # Implement Forward Propagation to calculate A2 (probabilities)
+        Z1 = np.dot(W1, X) + b1
+        A1 = np.tanh(Z1)
+        Z2 = np.dot(W2, A1) + b2
+        A2 = sigmoid(Z2)
+        assert (A2.shape == (1, X.shape[1]))
+        self.cache = {"Z1": Z1,
+                 "A1": A1,
+                 "Z2": Z2,
+                 "A2": A2}
+
+
+
+
+
